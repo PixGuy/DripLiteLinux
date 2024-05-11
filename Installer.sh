668780cd5438c15e65b5beca057e39cb44861d06
PixGuy <cybercampos.assistencia@gmail.com>
2024-05-11 00:47:19 -0300

    Changed all files to match Drip Lite installation

--- Installer.sh
+++ Installer.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-: ' CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE
-    LEGAL SERVICES. DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN
-    ATTORNEY-CLIENT RELATIONSHIP. CREATIVE COMMONS PROVIDES THIS
-    INFORMATION ON AN "AS-IS" BASIS. CREATIVE COMMONS MAKES NO WARRANTIES
-    REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS
-    PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM
-    THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED
-    HEREUNDER. '
-
 dpn=("wget" "unzip" "curl" "jq")
 jdkurl="https://cdn.azul.com/zulu/bin/zulu8.72.0.17-ca-fx-jdk8.0.382-win_x64.zip"
 jdkzip="$(pwd)/$(basename "$jdkurl")"
@@ -18,7 +9,7 @@ wineurl=$(curl -s -q https://api.github.com/repos/GloriousEggroll/wine-ge-custom
 winetar="$(pwd)/$(basename "$wineurl")"
 winefp=${winetar//"wine-"/}; winefp=${winefp//".tar.xz"/}
 
-echo "Augustus Installer" 
+echo "Drip Lite Installer"
 sleep 1
 
 echo "Checking for required dependencies..."
@@ -26,17 +17,42 @@ echo "Checking for required dependencies..."
 for dependency in "${dpn[@]}"; do
     if ! command -v "$dependency" &> /dev/null; then
         echo "$dependency is not installed!"
+        exit 1
     fi
 done
 sleep 3
 
-echo "Downloading AzulJDK..."
-wget -nv "$jdkurl" &> /dev/null
+echo "You need to download a portable Minecraft Launcher (for Windows) before running this script!"
+echo "If you have one, please move it to THIS folder!"
+echo "Do you have a portable Launcher installed, on this folder, ready to go? y/n"
+read -r Launcher
+if [[ ! $Launcher ]]; then
+    echo "Please provide an answer!"
+    exit 1
+fi
+Launcher="${Launcher,,}"
+if [[ $Launcher != "y" && $Launcher != "yes" ]]; then
+    echo "Install a Launcher first!"
+    exit 1
+else
+    echo "Please provide the location of the Launcher from this directory."
+    read -r LauncherLoc
+    if [[ ! $LauncherLoc ]]; then
+        echo "Please provide a location!"
+        exit 1
+    else
+        echo "Set Launcher location to $(pwd)/$LauncherLoc."
+    fi
+fi
 sleep 3
 
-echo "Unzipping AzulJDK..."
-unzip -qq "$jdkzip" &> /dev/null
-rm "$jdkzip"
+echo "You must have Drip Lite already installed before running this script!"
+echo "If you already have Drip Lite installed, on this folder, and ready to go, please enter the name of the executable."
+read -r DripExec
+if [[ ! $DripExec ]]; then
+    echo "Please enter the name of the executable!"
+    exit 1
+fi
 sleep 3
 
 echo "Downloading WineGE..."
@@ -48,11 +64,14 @@ tar -xf "$winetar" &> /dev/null
 rm "$winetar"
 sleep 3
 
-echo "Making Launch script..."
-echo "$winefp/bin/wine $jdkfp/bin/javaw.exe -jar $(pwd)/Launcher.jar" > Launcher.sh
+echo "Making Drip Lite script..."
+echo "$winefp/bin/wine $(pwd)/$DripExec" > Drip.sh
+chmod +x Drip.sh
+sleep 1
+
+echo "Making Launcher script..."
+echo "$winefp/bin/wine $(pwd)/$LauncherLoc" > Launcher.sh
 chmod +x Launcher.sh
 sleep 1
 
-echo "All done! Download the Augustus .jar Launcher (the alternative download) and place it here!"
-
-
+echo "All done!"
